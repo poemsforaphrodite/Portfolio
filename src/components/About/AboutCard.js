15c3edebf9284f4fe2f20c3daeb26909266db5dc
zyneman <pushpendersolanki_co21a6_23@dtu.ac.in>
2024-03-05 02:36:23 +0530

    first commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Pushpender Solanki</span>
+            from <span className="purple"> New Delhi, India.</span>
+            <br />
+            I am currently pursuing bachelor's degree in Computer Science at DTU.
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writing Poems
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Learning New Technologies
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Pushpender</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
